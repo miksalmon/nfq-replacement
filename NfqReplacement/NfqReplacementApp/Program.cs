ae1b7721d1659a4ca0ab41e3f5805d6b136d5bf3
Michael Salmon 🐟 <michaelpsalmon@outlook.com>
2025-03-05 14:06:11 -0500

    remove dbbindings

--- NfqReplacement/NfqReplacementApp/Program.cs
+++ NfqReplacement/NfqReplacementApp/Program.cs
@@ -39,7 +39,6 @@ internal class Program
         foreach (var file in files)
         {
             Console.WriteLine($"Name: {file.Name}");
-            Console.WriteLine($"Path: {file.Path}");
             Console.WriteLine($"Kind: {file.Kind}");
             Console.WriteLine($"ItemDate: {file.ItemDate}");
             Console.WriteLine($"DateModified: {file.DateModified}");
